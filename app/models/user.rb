0fc18af610e35a902c689aec5d9c281396bc5491
ai80y <a25101ya@aiit.ac.jp>
2026-04-24 18:08:41 +0000

    Add build script

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :microposts
+  validates （コードを書き込む）, presence: true # （aaaaaaa）の中身を書き換えてください
+  validates （コードを書き込む）, presence: true # （bbbbbbbbbbgit status ）の中身を書き換えてください
+end
